8c6a1622fdfd48c8cb6e5a6bc55433e793bf24f1
Mitch <mitchem890@gmail.com>
2020-08-11 20:05:31 -0500

    added Test to validate_user_input

---
+++ analysis/tests/test_Validate_User_Input.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+from classes import Images
+from classes import TaskGLMs
+from utils import Validate_User_Input
+import pytest
+
+
+@pytest.mark.parameterize("pipeline",[("hcp"),("fmriprep")])
+def test_validate_pipeline(pipeline):
+
+    assert Validate_User_Input.validate_pipeline(pipeline) is None
+
+def test_validate_pipeline_error():
+    with pytest.raises(IOError):
+        assert Validate_User_Input.validate_pipeline("badInput")
